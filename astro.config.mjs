2699461f036e2c8d77e5a23620305323d1841e6a
Trevor Huis in 't Veld <trevorh@hey.com>
2025-07-21 23:04:18 -0500

    add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   image: {
     layout: "full-width",
   },
+
+  adapter: cloudflare(),
 });
